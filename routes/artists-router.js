61b37cc692cc5677940ce5d6da4754a44a8257e1
Your Name <plumge@gmail.com>
2019-10-30 13:31:01 -0400

    resolve conflicts

--- routes/artists-router.js
+++ routes/artists-router.js
@@ -5,4 +5,6 @@ const ArtistController = require("../controllers/artists-controller")
 
 router.get("/", ArtistController.getAllArtists);
 
+router.post("/", ArtistController.addNewArtist);
+
 module.exports = router;
