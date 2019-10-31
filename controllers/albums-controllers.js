4864ae09abd02b09618aaab3e9adb5dbbec6d3a7
Cordell <muhammadcordell@gmail.com>
2019-10-31 14:16:55 -0400

    cm

--- controllers/albums-controllers.js
+++ controllers/albums-controllers.js
@@ -2,7 +2,7 @@ const AlbumsService = require("../services/albums-service");
 
 module.exports = {
     addNewAlbums(req, res) {
-        const {title, image, songs, recordLabel}
+        const {title, image, songs, recordLabel } = req.body;
 
         AlbumsService.add(title, image, songs, recordLabel => {
             res.json({respone});
