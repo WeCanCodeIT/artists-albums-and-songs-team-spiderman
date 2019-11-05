e0884f10e16341cbd36a76ec001017ea1fe43fb7
Your Name <plumge@gmail.com>
2019-11-05 14:37:24 -0500

    divided into front-end and back-end folders

--- models/artist-domain.js
+++
@@ -1,8 +0,0 @@
-class Artist {
-    constructor (name , image, recordLabel) {
-        this.name = name;
-        this.image = image;
-        this.recordLabel = recordLabel;
-    }
-}
-module.exports = Artist; 
--- routes/index-router.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// GET Home Page
-router.get("/", function(req, res, next) {
-  res.redirect("/artists");
-});
-
-module.exports = router;
