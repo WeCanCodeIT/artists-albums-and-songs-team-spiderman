391e96a674f3b2a5df1de9c2c8c2856f99cfd453
Emi <emi.bungo@gmail.com>
2019-11-11 11:51:49 -0500

    Added cors in app.js

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 require("./utils/db");
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const cors = require("cors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const app = express();
 
-const indexRouter = require('./routes/index-router');
-const artistsRouter = require('./routes/artists-router');
-const albumsRouter = require('./routes/albums-router');
-const songRouter = require('./routes/song-router');
+const indexRouter = require("./routes/index-router");
+const artistsRouter = require("./routes/artists-router");
+const albumsRouter = require("./routes/albums-router");
+const songRouter = require("./routes/song-router");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/artists', artistsRouter);
-app.use('/albums', albumsRouter);
-app.use('/songs', songRouter);
+app.use("/", indexRouter);
+app.use("/artists", artistsRouter);
+app.use("/albums", albumsRouter);
+app.use("/songs", songRouter);
 module.exports = app;
